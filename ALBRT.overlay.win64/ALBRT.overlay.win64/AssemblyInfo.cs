513be7b9d3571f9f4ace4d8df595af7be1c1f1bc
John Penny <johnpenny+work@gmail.com>
2023-07-14 23:12:22 +0100

    v

--- ALBRT.overlay.win64/ALBRT.overlay.win64/AssemblyInfo.cs
+++ ALBRT.overlay.win64/ALBRT.overlay.win64/AssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyProduct("ALBRT Overlay")]
 [assembly: AssemblyCompany("ALBRT.VR")]
 [assembly: AssemblyCopyright("Copyright 2023 ALBRT.VR contributors. All rights reserved.")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyInformationalVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 
 // NOTE prevent the useless defaults being set inside your project file xaml:
 
